838150f1a1a72836cae76f7f51648da3557c81f8
ConnorGuidry <connor.guidry@gmail.com>
2019-04-20 15:16:14 -0500

    created getEncryptedString and getDecryptedString methods

--- src/cipherassist/encryption/EncryptionMethod.java
+++ src/cipherassist/encryption/EncryptionMethod.java
@@ -1,6 +1,20 @@
 package cipherassist.encryption;
 
+/** 
+ * @author ConnorGuidry
+ *
+ */
 public interface EncryptionMethod 
 {
-
+	/** Takes plaintext string and key string
+	 * @return Returns encrypted text as String
+	 */
+	public String getEncryptedString(String plaintext, String key);
+	
+	
+	/** Takes ciphertext string and key string
+	 * @return Returns decrypted text as String
+	 */
+	public String getDecryptedString(String ciphertext, String key);
+	
 }
