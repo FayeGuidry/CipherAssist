c37f42f391416e0f0aeddd2b265f072f51dad638
ConnorGuidry <connor.guidry@gmail.com>
2019-04-20 15:23:49 -0500

    created ROT13 class

---
+++ src/cipherassist/encryption/ROT13.java
@@ -0,0 +1,18 @@
+package cipherassist.encryption;
+
+public class ROT13 implements EncryptionMethod
+{
+
+	@Override
+	public String getEncryptedString(String plaintext, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDecryptedString(String ciphertext, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
