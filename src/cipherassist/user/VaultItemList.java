b0121aec418f83d5b2a9f54038bf0b6a110cfe9d
ConnorGuidry <connor.guidry@gmail.com>
2019-04-21 14:58:50 -0500

    Created VaultItemList class and empty methods

---
+++ src/cipherassist/user/VaultItemList.java
@@ -0,0 +1,54 @@
+package cipherassist.user;
+
+public class VaultItemList 
+{
+	public void add(VaultItem newEntry)
+	{
+		
+	}
+	
+	public void add(int newPosition, VaultItem newEntry)
+	{
+		
+	}
+	
+	public VaultItem remove(int givenPosition)
+	{
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public VaultItem replace(int givenPosition, VaultItem newEntry)
+	{
+		
+	}
+	
+	public VaultItem getEntry(int givenPosition)
+	{
+		
+	}
+	
+	public VaultItem[] toArray()
+	{
+		
+	}
+	
+	public boolean contains(VaultItem anEntry)
+	{
+		
+	}
+	
+	public int getLength()
+	{
+		
+	}
+	
+	public boolean isEmpty()
+	{
+		
+	}
+}
