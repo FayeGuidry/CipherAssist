5db8f6e4765988443706b8ac40c87aa49998d462
ConnorGuidry <connor.guidry@gmail.com>
2019-04-21 18:43:34 -0500

    Created AES class and added state field

---
+++ src/cipherassist/encryption/AES.java
@@ -0,0 +1,26 @@
+package cipherassist.encryption;
+
+public class AES extends EncryptionBase implements EncryptionMethod
+{	
+	private byte[][] state = new byte[4][4];
+	
+	@Override
+	public String getEncryptedString(String plaintext) {
+	
+		return null;
+	}
+
+	@Override
+	public String getDecryptedString(String ciphertext) {
+		
+		return null;
+	}
+
+	@Override
+	public void getKey(String key) {
+		
+	}
+	
+	
+	
+}
