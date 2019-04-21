cad5f96585ad07d2f1d007f7f267366f7fe95679
ConnorGuidry <connor.guidry@gmail.com>
2019-04-21 17:37:55 -0500

    Fixed code warning after edit

--- src/cipherassist/fileio/CipherIO.java
+++ src/cipherassist/fileio/CipherIO.java
@@ -1,7 +1,6 @@
 package cipherassist.fileio;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
