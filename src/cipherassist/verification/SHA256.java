e146533e736a6d8ec6eda316eca570b32b4857d8
ConnorGuidry <connor.guidry@gmail.com>
2019-04-22 18:43:45 -0500

    Added working variables and empty constructor

--- src/cipherassist/verification/SHA256.java
+++ src/cipherassist/verification/SHA256.java
@@ -2,13 +2,32 @@ package cipherassist.verification;
 
 public class SHA256 
 {
-	private final int H0 = 0x6a09e667;
-	private final int H1 = 0xbb67ae85;
-	private final int H2 = 0x3c6ef372;
-	private final int H3 = 0xa54ff53a;
-	private final int H4 = 0x510e527f;
-	private final int H5 = 0x9b05688c;
-	private final int H6 = 0x1f83d9ab;
-	private final int H7 = 0x5be0cd19;
+	//Variables that hold the hash values
+	private int h0 = 0x6a09e667;
+	private int h1 = 0xbb67ae85;
+	private int h2 = 0x3c6ef372;
+	private int h3 = 0xa54ff53a;
+	private int h4 = 0x510e527f;
+	private int h5 = 0x9b05688c;
+	private int h6 = 0x1f83d9ab;
+	private int h7 = 0x5be0cd19;
 	
+	//Working variables
+	private int a;
+	private int b;
+	private int c;
+	private int d;
+	private int e;
+	private int f;
+	private int g;
+	private int h;
+	
+	//N = number of blocks in the padded message
+	private int N;
+	
+	
+	public SHA256()
+	{
+		
+	}
 }
