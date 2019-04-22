c9f7e4145697e0f4077e3c11141ea3cef856061f
ConnorGuidry <connor.guidry@gmail.com>
2019-04-22 18:28:06 -0500

    Created SHA256 class, set initial hash values

---
+++ src/cipherassist/verification/SHA256.java
@@ -0,0 +1,14 @@
+package cipherassist.verification;
+
+public class SHA256 
+{
+	private final int H0 = 0x6a09e667;
+	private final int H1 = 0xbb67ae85;
+	private final int H2 = 0x3c6ef372;
+	private final int H3 = 0xa54ff53a;
+	private final int H4 = 0x510e527f;
+	private final int H5 = 0x9b05688c;
+	private final int H6 = 0x1f83d9ab;
+	private final int H7 = 0x5be0cd19;
+	
+}
