694eff20381683e1638a2d24848850c331e6d757
ConnorGuidry <connor.guidry@gmail.com>
2019-04-22 19:56:04 -0500

    Added and implemented string/byte conversion methods to SHA256

--- src/cipherassist/verification/SHA256.java
+++ src/cipherassist/verification/SHA256.java
@@ -12,7 +12,7 @@ public class SHA256
 	private int h6 = 0x1f83d9ab;
 	private int h7 = 0x5be0cd19;
 	
-	//Working variables
+	//Working variables, 32-bit words used to compute hash values
 	private int a;
 	private int b;
 	private int c;
@@ -22,6 +22,7 @@ public class SHA256
 	private int g;
 	private int h;
 	
+	//Constants used for iterations of hash computation
 	private static final int K[] = new int[] { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 			   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 			   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -38,5 +39,32 @@ public class SHA256
 	public SHA256()
 	{
 		
+	}
+	
+	private void preProcess()
+	{
+		
+	}
+	
+	private void computeHash()
+	{
+		
+	}
+	
+	private void padMessage()
+	{
+		
+	}
+	
+	private String byteArrayToString(byte[] bytes)
+	{
+		String str = new String(bytes);
+		return str;
+	}
+	
+	private byte[] stringToByteArray(String str)
+	{
+		byte[] bytes = str.getBytes();
+		return bytes;
 	}
 }
