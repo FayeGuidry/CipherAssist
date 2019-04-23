a99d441734cd0a954ddd5594fdd446fc091566bc
ConnorGuidry <connor.guidry@gmail.com>
2019-04-23 11:37:41 -0500

    Added initial comments in Binary class

--- src/ConnorGuidry/BinaryUtil/Binary.java
+++ src/ConnorGuidry/BinaryUtil/Binary.java
@@ -1,5 +1,8 @@
 package ConnorGuidry.BinaryUtil;
 
-public class Binary {
-
+//Created by ConnorGuidry
+//Binary is a String-based binary-holding data type that allows the direct and simple manipulation of bits in Java. 
+public class Binary 
+{
+	
 }
