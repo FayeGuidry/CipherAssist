20b7a1403066ccfcc9524e7953353e82057c9871
ConnorGuidry <connor.guidry@gmail.com>
2019-04-23 11:51:26 -0500

    Implemented constructor for Binary class

--- src/ConnorGuidry/BinaryUtil/Binary.java
+++ src/ConnorGuidry/BinaryUtil/Binary.java
@@ -5,4 +5,24 @@ package ConnorGuidry.BinaryUtil;
 public class Binary 
 {
 	
+	String binaryString = null;
+	
+	public Binary(String binaryString)
+	{
+		for (int count = 0; count < binaryString.length(); ++count)
+		{
+			if ((binaryString.charAt(count) != '1') || (binaryString.charAt(count) != '0'))
+			{
+				count = binaryString.length() + 1;
+				throw new IllegalArgumentException("Binary data type can only accept Strings consisting of either 1 or 0.");
+			}
+		}
+		
+		this.binaryString = binaryString;
+	}
+	
+	public static Binary leftShift()
+	{
+		
+	}
 }
