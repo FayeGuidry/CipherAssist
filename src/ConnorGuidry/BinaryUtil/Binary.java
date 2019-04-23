3525bbd20c00a3e304184defe3e21780d89a6d5e
ConnorGuidry <connor.guidry@gmail.com>
2019-04-23 11:55:09 -0500

    Added getter and setter for binaryString field in Binary class

--- src/ConnorGuidry/BinaryUtil/Binary.java
+++ src/ConnorGuidry/BinaryUtil/Binary.java
@@ -9,20 +9,31 @@ public class Binary
 	
 	public Binary(String binaryString)
 	{
-		for (int count = 0; count < binaryString.length(); ++count)
-		{
-			if ((binaryString.charAt(count) != '1') || (binaryString.charAt(count) != '0'))
-			{
-				count = binaryString.length() + 1;
-				throw new IllegalArgumentException("Binary data type can only accept Strings consisting of either 1 or 0.");
-			}
-		}
-		
-		this.binaryString = binaryString;
+		setBinaryString(binaryString);
 	}
 	
 	public static Binary leftShift()
 	{
 		
+	}
+	
+	public String getBinaryString()
+	{
+		return binaryString;
+	}
+	
+	public void setBinaryString(String binaryString)
+	{
+		//Checks if param binaryString only consists of 1's or 0's
+				for (int count = 0; count < binaryString.length(); ++count)
+				{
+					if ((binaryString.charAt(count) != '1') || (binaryString.charAt(count) != '0'))
+					{
+						count = binaryString.length() + 1;
+						throw new IllegalArgumentException("Binary data type can only accept Strings consisting of either 1 or 0.");
+					}
+				}
+				
+				this.binaryString = binaryString;
 	}
 }
