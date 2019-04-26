8b3234f72bf601fcd0d72e342f7f66915c4937f5
KheKit <khekit@hotmail.com>
2019-04-25 23:10:00 -0500

    added constructor

--- src/cipherassist/verification/ArrayList.java
+++ src/cipherassist/verification/ArrayList.java
@@ -1,11 +1,22 @@
 package cipherassist.verification;
 
-public class ArrayList 
+public class ArrayList<T>
 {
 	// simple array list implementation
 	private int INITIAL_CAPACITY = 20;
-	private String[] arraylist = new String[INITIAL_CAPACITY];
-	private int numberOfItems = 0;
+	private T[] itemlist;
+	private int numberOfItems;
+	
+	public ArrayList()
+	{
+		this(0);
+	}
+	
+	public ArrayList(int num)
+	{
+		itemlist = (T[]) new Object[INITIAL_CAPACITY];
+		this.numberOfItems = 0;
+	}
 	
 	
 	
