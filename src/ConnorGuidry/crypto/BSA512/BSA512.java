c76b8ba8d30951933751edb984163e0fe9cd4174
ConnorGuidry <connor.guidry@gmail.com>
2019-04-26 20:14:58 -0500

    Added fields for message prep and began implementing constructor

--- src/ConnorGuidry/crypto/BSA512/BSA512.java
+++ src/ConnorGuidry/crypto/BSA512/BSA512.java
@@ -3,16 +3,52 @@ package ConnorGuidry.crypto.BSA512;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
+import ConnorGuidry.BinaryUtil.Binary;
+
 public class BSA512 
 {
-	public BSA512()
+	public char[] charArray;
+	
+	public int paddedMessageLength = 0;
+	
+	public Binary message;
+	
+	public int zerosToAdd;
+	
+	public BSA512(String plaintext, String key) throws UnsupportedEncodingException
+	{
+		message = new Binary(messageToBinary(plaintext));
+	}
+	
+	public String encrypt()
 	{
 		
 	}
 	
-	public String getPaddedMessage(String unpaddedMessage)
+	public String decrypt()
 	{
 		
+	}
+	
+	
+	public String getPaddedMessage(String unpaddedMessage)
+	{
+		Binary onePadding = new Binary("1");
+
+		String zeros = "";
+		
+		for (int i = 0; i < zerosToAdd; ++i)
+		{
+			zeros += '0';
+		}
+		
+		Binary zeroPadding = new Binary(zeros);
+		
+		
+		//System.out.println("One padding: " + onePadding);
+		//System.out.println("Zero padding: " + zeroPadding);
+		this.M.append(onePadding);
+		this.M.append(zeroPadding);
 	}
 	
 	private String messageToBinary(String message) throws UnsupportedEncodingException
