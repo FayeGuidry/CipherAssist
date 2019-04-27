3cc744d1a2908f9ef28821ef70907e33702a4202
KheKit <khekit@hotmail.com>
2019-04-27 16:23:49 -0500

    structure for queue

---
+++ src/cipherassist/encryption/LinkedQueue.java
@@ -0,0 +1,80 @@
+package cipherassist.encryption;
+
+public class LinkedQueue<T> implements QueueInterface<T>
+{
+	private Node firstNode;
+	private Node lastNode;
+	
+	public boolean add(T newEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offer(T newEntry) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	private class Node
+	{
+		private T data;
+		private Node next;
+		private Node prev;
+		
+		public Node()
+		{
+			this(null, null, null);
+		}
+		
+		public Node(T data, Node next, Node prev)
+		{
+			this.data = data;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
+}
