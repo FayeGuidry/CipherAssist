bede157b9736fe54007e7196bcfd9870ba569ab6
KheKit <khekit@hotmail.com>
2019-04-27 12:56:26 -0500

    added constructor and push method

--- src/cipherassist/encryption/ArrayStack.java
+++ src/cipherassist/encryption/ArrayStack.java
@@ -4,22 +4,47 @@ public class ArrayStack<T> implements StackInterface<T>
 {
 	private T[] aStack;
 	private int numOfEntries;
-	private static int INITIAL_CAPACITY;
+	private int topIndex;
+	private static final int DEFAULT_INITIAL_CAPACITY = 50;
 	
 	public ArrayStack()
 	{
-		this(INITIAL_CAPACITY);
+		this(DEFAULT_INITIAL_CAPACITY);
 	}
 	
 	public ArrayStack(int capacity)
 	{
-		aStack = (T[]) new Object[capacity];
+		// the cast is safe because the new array contains null entries
+		@SuppressWarnings("unchecked")
+		T[] tempStack = (T[]) new Object[capacity];
+		aStack = tempStack;
+		topIndex = -1;
+		numOfEntries = 0;
 	}
 
 	public void push(T newEntry) 
 	{
+		resizeArray();
+		topIndex++;
+		aStack[topIndex] = newEntry;
 		
-		
+	}
+	
+	private void resizeArray()
+	{
+		if (topIndex == aStack.length - 1) //if array is full,
+		{
+			//double size of array
+			@SuppressWarnings("unchecked")
+			T[] aNewStack = (T[]) new Object[2 * aStack.length];
+			for(int i = 0; i < aStack.length; i++)
+			{
+				aNewStack[i] = aStack[i];
+			}
+			aStack = aNewStack;
+		}
+			
+			
 	}
 	
 	public T pop() 
