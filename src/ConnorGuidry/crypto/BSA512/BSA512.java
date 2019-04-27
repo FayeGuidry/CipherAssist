851e782927d3122911e4eecad85e8a7ff0280182
ConnorGuidry <connor.guidry@gmail.com>
2019-04-26 20:06:13 -0500

    Added constructor, getPaddedMessage method and messageToBinary method to BSA512 class

--- src/ConnorGuidry/crypto/BSA512/BSA512.java
+++ src/ConnorGuidry/crypto/BSA512/BSA512.java
@@ -1,6 +1,39 @@
 package ConnorGuidry.crypto.BSA512;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+
 public class BSA512 
 {
-
+	public BSA512()
+	{
+		
+	}
+	
+	public String getPaddedMessage(String unpaddedMessage)
+	{
+		
+	}
+	
+	private String messageToBinary(String message) throws UnsupportedEncodingException
+	{
+		byte[] bytes = message.getBytes("UTF8");
+		String convertedToBinary = new BigInteger(bytes).toString(2);
+		
+		if (convertedToBinary.length() % 8 != 0)
+		{
+			int mod = convertedToBinary.length() % 8;
+			int zeros = 8 - mod;
+			String append = "";
+			
+			for (int i = 0; i < zeros; ++i)
+			{
+				append += "0";
+			}
+			
+			convertedToBinary = append + convertedToBinary;
+		}
+		
+		return convertedToBinary;
+	}
 }
